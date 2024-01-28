ab96282f49dceb046eea11100538f7a07cf27d1b
GnussonNet <admin@gnusson.net>
2024-01-28 20:40:47 +0100

    Added neo-tree buffer title

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,25 +1,25 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
 
-	config = function()
-		require("bufferline").setup({
-			options = {
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "",
-						separator = false,
-						text_align = "left",
-					},
-				},
-				diagnostics = "nvim_lsp",
-				separator_style = { "", "" },
-				modified_icon = "●",
-				show_close_icon = false,
-				show_buffer_close_icons = false,
-			},
-		})
-	end,
+  config = function()
+    require("bufferline").setup({
+      options = {
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Expolorer",
+            separator = true,
+            text_align = "center",
+          },
+        },
+        diagnostics = "nvim_lsp",
+        separator_style = { "", "" },
+        modified_icon = "●",
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+      },
+    })
+  end,
 }
