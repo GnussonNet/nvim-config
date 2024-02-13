7a1b1adf330640d36846a6ded2f40d04b3f58751
GnussonNet <admin@gnusson.net>
2024-02-13 22:09:27 +0100

    Hide bufferline when buffers < 2

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,10 +2,10 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
-
   config = function()
     require("bufferline").setup({
       options = {
+        always_show_bufferline = false,
         offsets = {
           {
             filetype = "neo-tree",
