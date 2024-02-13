fd3c68fef3926d04710e3fb162614cbdf7a3de91
GnussonNet <admin@gnusson.net>
2024-02-13 22:08:30 +0100

    Updated option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.o.number = true
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
@@ -20,27 +21,40 @@ opt.ignorecase = true      -- Ignore case
 opt.inccommand = "nosplit" -- preview incremental substitute
 opt.laststatus = 3         -- global statusline
 opt.list = true            -- Show some invisible characters (tabs...
-opt.mouse = "a"            -- Enable mouse mode
-opt.number = true          -- Print line number
 opt.pumblend = 10          -- Popup blend
 opt.pumheight = 10         -- Maximum number of entries in a popup
 opt.relativenumber = true  -- Relative line numbers
+opt.number = true          -- Print line number
 opt.scrolloff = 8          -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true      -- Round indent
-opt.shiftwidth = 2         -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false       -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8      -- Columns of context
-opt.signcolumn = "yes"     -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true       -- Don't ignore case with capitals
-opt.smartindent = true     -- Insert indents automatically
+opt.sessionoptions = {
+  "buffers",
+  "curdir",
+  "tabpages",
+  "winsize",
+  "help",
+  "globals",
+  "skiprtp",
+  "folds"
+}
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2    -- Size of an indent
+opt.shortmess:append({
+  W = true,
+  I = true,
+  c = true,
+  C = true
+})
+opt.showmode = false     -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8    -- Columns of context
+opt.signcolumn = "yes"   -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true     -- Don't ignore case with capitals
+opt.smartindent = true   -- Insert indents automatically
 opt.spelllang = { "en" }
-opt.splitbelow = true      -- Put new windows below current
+opt.splitbelow = true    -- Put new windows below current
 opt.splitkeep = "screen"
-opt.splitright = true      -- Put new windows right of current
-opt.tabstop = 2            -- Number of spaces tabs count for
-opt.termguicolors = true   -- True color support
+opt.splitright = true    -- Put new windows right of current
+opt.tabstop = 2          -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
 opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
