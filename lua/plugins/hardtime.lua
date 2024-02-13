00e66aebe08ff8284b50dfbc3ab245598974016e
GnussonNet <admin@gnusson.net>
2024-02-13 22:40:45 +0100

    Added neo-tree-popup to disabled_filetypes

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,7 +3,14 @@ return {
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   config = function()
     require("hardtime").setup({
-      disabled_filetypes = { "neo-tree", "lazy", "mason", "dashboard"},
+      disabled_filetypes = {
+        "qf",
+        "netrw",
+        "neo-tree",
+        "neo-tree-popup",
+        "lazy",
+        "mason",
+        "dashboard" },
     })
   end
 }
