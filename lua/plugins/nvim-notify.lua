27be006b068830c29b0c95477c8bf6fd846e8be9
GnussonNet <admin@gnusson.net>
2024-11-28 16:28:33 +0100

    Added background color to toasts

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,7 +1,9 @@
 return {
   "rcarriga/nvim-notify",
-  lazy = false,
+  event = "VeryLazy",
   config = function()
-    require("notify").setup({})
+    require("notify").setup({
+      background_colour = "#1e222a",
+    })
   end
 }
