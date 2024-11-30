b920730698f70dcb4a4e18efeaf8e49d6c859545
GnussonNet <admin@gnusson.net>
2024-11-30 22:19:31 +0100

    Changed defautl filtered items

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -31,6 +31,16 @@ return {
           },
         },
       },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            "thumbs.db"
+          },
+        }
+      },
       window = {
         position = "right",
         mappings = {
