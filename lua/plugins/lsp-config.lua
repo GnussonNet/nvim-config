55998ffc8fcda60c3ba1a7a7cf97458d4550d238
GnussonNet <admin@gnusson.net>
2025-10-03 14:38:16 +0200

    Formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,54 +1,66 @@
 return {
-	"neovim/nvim-lspconfig",
+  "neovim/nvim-lspconfig",
 
-	dependencies = {
-		"mason-org/mason.nvim",
-		"mason-org/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"saghen/blink.cmp",
-	},
+  dependencies = {
+    "mason-org/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "saghen/blink.cmp",
+  },
 
-	config = function()
-		local mason_servers = {
-			"bashls",
-			"cssls",
-			"html",
-			"jsonls",
-			"lua_ls",
-			"tailwindcss",
-			"ts_ls",
-		}
+  config = function()
+    local mason_servers = {
+      "bashls",
+      "cssls",
+      "html",
+      "jsonls",
+      "lua_ls",
+      "tailwindcss",
+      "ts_ls",
+    }
 
-		local mason_tools = {
-			"eslint_d",
-			"prettierd",
-			"stylua",
-		}
+    local mason_tools = {
+      "eslint_d",
+      "prettierd",
+      "stylua",
+    }
 
-		local servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			},
-		}
+    local servers = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      },
+    }
 
-		require("mason").setup()
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({ automatic_installation = false, ensure_installed = mason_servers })
-		require("mason-tool-installer").setup({ ensure_installed = mason_tools })
+    require("mason-lspconfig").setup({
+      automatic_installation = false,
+      ensure_installed = mason_servers,
+    })
+    require("mason-tool-installer").setup({ ensure_installed = mason_tools })
 
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend(
+      "force",
+      capabilities,
+      require("blink.cmp").get_lsp_capabilities({}, false)
+    )
 
-		-- 0.11 fix
-		for server, config in pairs(servers) do
-			config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, config.capabilities or {})
-			vim.lsp.config(server, config)
-		end
-	end,
+    -- 0.11 fix
+    for server, config in pairs(servers) do
+      config.capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        capabilities,
+        config.capabilities or {}
+      )
+      vim.lsp.config(server, config)
+    end
+  end,
 }
